a0ce4315513eb047f7e089de034fe1497ae7a931
lckdscl <lckdscl+github@protonmail.com>
2022-02-16 09:37:03 +0000

    Added all files, data and scripts

---
+++ student_solar_data_2016/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for dir in ./*/
+do
+    if [ ! -e "${dir}/data.txt" ]; then
+        touch "${dir}/data.txt"
+        dirclean=${dir%*/}
+        dirclean="${dirclean##*/}"
+        dirclean=$(echo "${dirclean}" | cut -d"_" -f2,3)
+        echo "${dirclean}" > "./${dir}/data.txt"
+    fi
+done
---
+++ student_solar_data_2016/solar_2016-10-05_seq1/convert.bat
@@ -0,0 +1,1 @@
+for %%f in (*.bmp) do (convert.exe %%f %%~nf.fits)
