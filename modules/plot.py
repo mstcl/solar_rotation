fc7803a3142321313022e51962f84bf79c733ebd
lckdscl <lckdscl+github@protonmail.com>
2022-02-22 16:06:23 +0000

    Edit: reformat

--- modules/plot.py
+++ modules/plot.py
@@ -10,14 +10,16 @@ def plot_equator(x_arr: np.ndarray, y_arr: np.ndarray, sequence: str, line: tupl
     """
     Plot using matplotlib and save to directory
     """
-    y_int = line[0]*x_arr+line[1]
+    y_int = line[0] * x_arr + line[1]
     plt.plot(x_arr, y_arr, "xb", label="data points")
-    plt.plot(x_arr, y_int, "r-", label="interpolated") # TODO: errors of the circles drawn
-    plt.ylabel(r'Y-coordinate of centre', fontsize=15)
-    plt.xlabel(r'X-coordinate of centre', fontsize=15)
-    plt.title('Heliocentric drift of the Earth due to the apparent movement of the Sun')
-    plt.legend(loc='upper right')
-    plt.errorbar(x_arr, y_arr, yerr=5, xerr=5, fmt="x",capsize=2)
+    plt.plot(
+        x_arr, y_int, "r-", label="interpolated"
+    )  # TODO: errors of the circles drawn
+    plt.ylabel(r"Y-coordinate of centre", fontsize=15)
+    plt.xlabel(r"X-coordinate of centre", fontsize=15)
+    plt.title("Heliocentric drift of the Earth due to the apparent movement of the Sun")
+    plt.legend(loc="upper right")
+    plt.errorbar(x_arr, y_arr, yerr=5, xerr=5, fmt="x", capsize=2)
     plt.savefig(f"./{sequence}/equatorial_plane.png", format="png", dpi=150)
     return True
 
