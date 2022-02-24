83b25d5ee640712717dc3d999146593f697a1fca
lckdscl <lckdscl+github@protonmail.com>
2022-02-24 00:26:08 +0000

    Edit: Refactor write_data()

--- modules/helper.py
+++ modules/helper.py
@@ -6,7 +6,8 @@ Frequently used functions
 from os.path import exists as file_exists
 import numpy as np
 
-def write_data(heading:str, values, sequence:str):
+
+def write_data(heading: str, values, sequence: str):
     """
     Write to data.txt the radii of the sunspots
     """
@@ -15,30 +16,17 @@ def write_data(heading:str, values, sequence:str):
             f"./{sequence}/data.txt was not found in this sequence. Run init.sh first."
         )
     with open(f"./{sequence}/data.txt", "r", encoding="utf-8") as old_file:
-        data = old_file.readlines()
-
-    changed = False
+        data = {
+            line.split(" ")[0]: " ".join(line.split(" ")[1:])
+            for line in old_file.readlines()
+        }
     if isinstance(values, np.ndarray):
         for j, val in enumerate(values):
-            for i, line in enumerate(data):
-                if "".join(line[0:2]) == f"{heading}{j+1}":
-                    data[i] = f"{heading}{j+1} {val}\n"
-                    changed = True
-                    break
-        if not changed:
-            for j, val in enumerate(values):
-                data.append(f"{heading}{j+1} {val}\n")
+            data[f"{heading}{j+1}"] = f"{val}\n"
     else:
-        for i, line in enumerate(data):
-            if line[0] == heading:
-                data[i] = f"{heading} {values}\n"
-                changed = True
-                break
-        if not changed:
-            data.append(f"{heading} {values}\n")
-
+        data[f"{heading}"] = f"{values}\n"
     with open(f"./{sequence}/data.txt", "w", encoding="utf-8") as new_file:
-        new_file.writelines(data)
+        new_file.writelines([" ".join(line) for line in data.items()])
     return True
 
 
@@ -47,6 +35,7 @@ def warn_module():
     Print a warning for modules that are not supposed to be executed
     """
     print("This file is not supposed to be executed directly")
+
 
 if __name__ == "__main__":
     warn_module()
