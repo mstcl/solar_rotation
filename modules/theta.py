e4a31f11ee3e5cab3ddc3ee58f104169381adffd
lckdscl <lckdscl+github@protonmail.com>
2022-02-24 22:21:05 +0000

    Edit: return all angles instead of the average

--- modules/theta.py
+++ modules/theta.py
@@ -7,24 +7,27 @@ import numpy as np
 from . import helper
 
 
-def get_angle(r_x:float, r_y:float, normal:float):
+def get_angle(r_x: np.ndarray, r_y: np.ndarray, normal: float):
     """
     Determine theta from the sign of r_y and normal
     """
-    angle = (
-        np.arccos(
-            (((r_x**2 + r_y**2) * (normal**2 + 1)) ** (-0.5))
-            * (r_x + (r_y * normal))
+    angles = []
+    for component in zip(r_x, r_y):
+        angle = (
+            np.arccos(
+                (((component[0]**2 + component[1]**2) * (normal**2 + 1)) ** (-0.5))
+                * (component[0] + (component[1] * normal))
+            )
+            * 180
+            / np.pi
         )
-        * 180
-        / np.pi
-    )
-    if angle > 180:
-        angle = angle - 180
-
-    if r_y > 0:
-        return angle if normal > 0 else (360 - angle)
-    return (360 - angle) if normal > 0 else angle
+        if angle > 180:
+            angle = angle - 180
+        if (component[1] > 0 and normal > 0) or (component[1] < 0 and normal < 0):
+            angles.append(angle)
+        else:
+            angles.append(360 - angle)
+    return np.array(angles)
 
 
 if __name__ == "__main__":
