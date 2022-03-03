686fc8ee8b1b2567f5a66c612951ffb242952774
lckdscl <lckdscl+github@protonmail.com>
2022-03-03 14:15:22 +0000

    Edit: remove legends and add in units

--- modules/plot.py
+++ modules/plot.py
@@ -15,10 +15,11 @@ def plot_equator(x_arr: np.ndarray, y_arr: np.ndarray, sequence: str, line: tupl
     plt.plot(
         x_arr, y_int, "r-", label="interpolated"
     )
-    plt.ylabel(r"Y-coordinate of centre", fontsize=15)
-    plt.xlabel(r"X-coordinate of centre", fontsize=15)
+    plt.ylabel(r"Y-coordinate of centre / pixel", fontsize=15)
+    plt.xlabel(r"X-coordinate of centre / pixel", fontsize=15)
     plt.title("Heliocentric drift of the Earth due to the apparent movement of the Sun")
-    plt.legend(loc="upper right")
+    # Uncomment for legend
+    # plt.legend(loc="upper right")
     plt.errorbar(x_arr, y_arr, yerr=5, xerr=5, fmt="x", capsize=2)
     plt.savefig(f"./{sequence}/equatorial_plane.png", format="png", dpi=150)
     return True
