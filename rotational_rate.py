5d162409ee10377209be07f95853c46c65ecb5b9
lckdscl <lckdscl+github@protonmail.com>
2022-03-08 20:01:45 +0000

    Completed sequences 2: electric boogaloo

--- rotational_rate.py
+++ rotational_rate.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 """
 Calculate the rotational rate and period from data
 """
 import numpy as np
+from modules import helper
 
 
 def get_rotational_frequency(latitude: float, constants: dict):
@@ -28,14 +30,18 @@ def get_frequency_error(latitude: float, latitude_error: float, constants: dict)
     Return the error of rotational frequency
     """
     latitude = latitude * np.pi / 180
-    return (
-        constants["A"][1] ** 2
-        + (np.sin(latitude) ** 2) * constants["B"][1] ** 2
-        + (np.sin(latitude) ** 4) * constants["C"][1] ** 2
-        + (constants["B"][0] + 2 * constants["C"] * (np.sin(latitude) ** 2))
-        * (np.sin(2 * latitude)) ** 2
+    in_a = float(constants["A"][1]) ** 2
+    in_b = (np.sin(latitude) ** 2) * float(constants["B"][1]) ** 2
+    in_c = (np.sin(latitude) ** 4) * float(constants["C"][1]) ** 2
+    in_l = (
+        (
+            float(constants["B"][0])
+            + 2 * float(constants["C"][0]) * (np.sin(latitude) ** 2)
+        )
         * latitude_error
-    ) ** (1 / 2)
+        * (np.sin(2 * latitude)) ** 2
+    )
+    return (in_a**2 + in_b**2 + in_c**2 + in_l**2) ** (1 / 2)
 
 
 def get_period_error(frequency: float, frequency_error: float):
@@ -49,8 +55,9 @@ def driver():
     """
     Fetch all quantities
     """
-    latitude = 0
-    latitude_error = 1
+    latitudes = np.array([22, 23, 26, 27])
+    latitude = float(helper.find_average(latitudes))
+    latitude_error = helper.find_std(latitudes, len(latitudes))
     constants = {
         "A": (14.713, 0.0491),
         "B": (-2.396, 0.188),
@@ -59,8 +66,10 @@ def driver():
     sidereal_correction = 360 / 365.25
     frequency = get_rotational_frequency(latitude, constants)
     frequency_error = get_frequency_error(latitude, latitude_error, constants)
-    period = get_period(frequency) * sidereal_correction
+    period = get_period(frequency) - sidereal_correction
     period_error = get_period_error(frequency, frequency_error)
+    print(f"freq:{frequency} +/- {frequency_error*frequency}")
+    print(f"period:{period} +/- {period_error*period}")
 
 
 if __name__ == "__main__":
