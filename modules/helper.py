ff8b583abd6d741d80f319fd6f4f2eaad8f81bf8
lckdscl <lckdscl+github@protonmail.com>
2022-03-09 10:36:17 +0000

    Edit: move polyfit and os file check here

--- modules/helper.py
+++ modules/helper.py
@@ -7,14 +7,24 @@ from numpy.lib.function_base import average
 import numpy as np
 
 
+def get_line(x_arr: np.ndarray, y_arr: np.ndarray):
+    """Interpolate to first degree and return gradient"""
+    return np.polyfit(x_arr, y_arr, 1, full=True)
+
+
+def check_file(file_path: str):
+    """
+    Catch file not found errors
+    """
+    if not file_exists(file_path):
+        raise FileNotFoundError(f"{file_path} was not found.")
+
+
 def get_value(sequence: str, target: str):
     """
     Return the normal
     """
-    if not file_exists(f"./{sequence}/data.txt"):
-        raise FileNotFoundError(
-            f"./{sequence}/data.txt was not found in this sequence. Run init.sh first."
-        )
+    check_file(f"./{sequence}/data.txt")
     with open(f"./{sequence}/data.txt", "r", encoding="utf-8") as file:
         data = {
             line.split(" ")[0]: " ".join(line.split(" ")[1:])
@@ -25,6 +35,7 @@ def get_value(sequence: str, target: str):
         value is not None
     ), f"This value {target} was not found. Make sure to run the required functions first."
     return float(value)
+
 
 def find_std(data: np.ndarray, num: int):
     """
@@ -50,10 +61,7 @@ def write_data(heading: str, values, sequence: str):
     """
     Write to data.txt the radii of the sunspots
     """
-    if not file_exists(f"./{sequence}/data.txt"):
-        raise FileNotFoundError(
-            f"./{sequence}/data.txt was not found in this sequence. Run init.sh first."
-        )
+    check_file(f"./{sequence}/data.txt")
     with open(f"./{sequence}/data.txt", "r", encoding="utf-8") as old_file:
         data = {
             line.split(" ")[0]: " ".join(line.split(" ")[1:])
@@ -65,6 +73,21 @@ def write_data(heading: str, values, sequence: str):
     else:
         data[f"{heading}"] = f"{values}\n"
     with open(f"./{sequence}/data.txt", "w", encoding="utf-8") as new_file:
+        new_file.writelines([" ".join(line) for line in data.items()])
+    return True
+
+def write_data_final(spot: str, values):
+    """
+    Write to data.txt the radii of the sunspots
+    """
+    check_file("./dataset/values.txt")
+    with open("./dataset/values.txt", "r", encoding="utf-8") as old_file:
+        data = {
+            line.split(" ")[0]: " ".join(line.split(" ")[1:])
+            for line in old_file.readlines()
+        }
+    data[f"{spot}"] = f"{' '.join(values)}\n"
+    with open("./dataset/values.txt", "w", encoding="utf-8") as new_file:
         new_file.writelines([" ".join(line) for line in data.items()])
     return True
 
